4402ddf49a805268365c513ebe0f264508898b93
mohbadar <mohammadbadarhashimi@gmail.com>
2019-12-10 12:02:07 +0430

    initail

---
+++ src/main/java/af/asr/phoenixvirusscanner/PhoenixVirusscannerApplication.java
@@ -0,0 +1,13 @@
+package af.asr.phoenixvirusscanner;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PhoenixVirusscannerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PhoenixVirusscannerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/af/asr/phoenixvirusscanner/PhoenixVirusscannerApplicationTests.java
@@ -0,0 +1,13 @@
+package af.asr.phoenixvirusscanner;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PhoenixVirusscannerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
